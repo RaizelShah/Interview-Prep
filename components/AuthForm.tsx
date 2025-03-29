b93c568f5e4bb7847ab05e9106e3b1bd3def158a
Raizel Shah <raizelshah1103@gmail.com>
2025-03-30 01:47:13 +0530

    implement firebase auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,6 +10,9 @@ import Link from "next/link";
 import {toast} from "sonner";
 import FormField from "@/components/FormField";
 import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/firebase/client";
+import {signIn, signUp} from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -36,13 +39,39 @@ const AuthForm = ({type}: { type: FormType }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === "sign-up") {
+                const {name, email, password} = values;
+                const userCredentails = await createUserWithEmailAndPassword(auth, email, password);
+                const result = await signUp({
+                    uid: userCredentails.user.uid,
+                    name: name!,
+                    email,
+                    password,
+                })
+
+                if (!result?.success) {
+                    toast.error(result?.message);
+                    return;
+                }
+
                 toast.success("Account created successfully! Please Sign In");
                 router.push('/sign-in');
                 console.log('Sign up', values);
             } else {
+                const {email, password} = values;
+                const userCredentails = await signInWithEmailAndPassword(auth, email, password);
+                const idToken = await userCredentails.user.getIdToken();
+
+                if (!idToken) {
+                    toast.error("sign In failed!");
+                    return;
+                }
+
+                await signIn({
+                    email, idToken
+                })
                 toast.success("Sign In successfully!");
                 router.push('/');
                 console.log('sign in', values);
@@ -67,8 +96,10 @@ const AuthForm = ({type}: { type: FormType }) => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
                         {!isSignIn && (
                             <FormField control={form.control} name="name" label="Name" placeholder="Your Name"/>)}
-                        <FormField control={form.control} name="email" label="Email" placeholder="Your email address" type="email"/>
-                        <FormField control={form.control} name="password" label="Password" placeholder="Enter yout password" type="password"/>
+                        <FormField control={form.control} name="email" label="Email" placeholder="Your email address"
+                                   type="email"/>
+                        <FormField control={form.control} name="password" label="Password"
+                                   placeholder="Enter yout password" type="password"/>
                         <Button className="btn" type="submit">
                             {isSignIn ? 'Sign in' : 'Create an Account'}
                         </Button>
---
+++ firebase/client.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBDBsTGiwGNilZeeZiCB9D7zDwl-RopOco",
+    authDomain: "prepwise-8f126.firebaseapp.com",
+    projectId: "prepwise-8f126",
+    storageBucket: "prepwise-8f126.firebasestorage.app",
+    messagingSenderId: "31449152861",
+    appId: "1:31449152861:web:50dc225ac7626d32c2bcb4",
+    measurementId: "G-9ES85K19ZV"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
